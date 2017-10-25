36dc293b3058334da0ceabf3e221e1b62b28e981
Droobert <droodoyle89@gmail.com>
2017-10-25 10:51:28 -0400

    Added interactive thumbnail w/ animation.

--- PA2/pa2script.js
+++ PA2/pa2script.js
@@ -18,3 +18,15 @@ function checkTime(i) {
     if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
     return i;
 }
+
+//The snippet below is inspired by code from https://codepen.io/rm89/pen/aNOmzQ?editors=1000
+var container = document.querySelector("#contentContainer");
+var kitty = document.querySelector("#helloKitty");
+
+container.addEventListener("click", function(event) {
+	var xPosition = event.clientX - container.getBoundingClientRect().left - (kitty.clientWidth /2);
+	var yPosition = event.clientY - container.getBoundingClientRect().top - (kitty.clientHeight /2);
+	kitty.style.left = xPosition + "px";
+	kitty.style.top = yPosition + "px";
+	}
+);
